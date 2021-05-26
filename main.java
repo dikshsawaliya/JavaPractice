e23a669699440b4c13ed0a66637c504d0e0e0f9e
dikshsawaliya <diksh19csu098@ncuindia.edu>
2021-05-26 11:28:43 +0530

    First programme

---
+++ main.java
@@ -0,0 +1,12 @@
+public class  main
+    {
+    public static void main (String[] args) 
+
+{
+    String x = "Diksh";
+    String y ="Sawaliya";
+
+    System.out.println("x: " +x);
+    System.out.println("y: " +y);
+}
+    }
