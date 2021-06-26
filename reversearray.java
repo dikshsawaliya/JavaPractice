02196d8aa959cee72c7ab2f7df57e84ab3ccd0a2
dikshsawaliya <diksh19csu098@ncuindia.edu>
2021-06-26 21:01:10 +0530

    New

---
+++ reversearray.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class reversearray {
+
+public static void main(String[]args) 
+{
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Enter the number of elements: ");
+    int N = scanner.nextInt();
+    int arr[] = new int [N];
+
+    System.out.println("Enter th elements: ");
+
+    for(i=0; i<N; i++)
+    {
+        arr[i] = scanner.nextInt();
+    }
+
+   // List<Integer>arr = new ArrayList<>();
+
+}    
+}
