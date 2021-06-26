da8d31e1f8fb73336e38c8c80e68fd07a7e42a5a
dikshsawaliya <diksh19csu098@ncuindia.edu>
2021-06-26 21:22:10 +0530

    Corection

---
+++ sumarray.java
@@ -0,0 +1,17 @@
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class sumarray 
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the size of array: ");
+
+    int n
+    sum = 0;
+
+    n = scanner.nextInt();
+    
