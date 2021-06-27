64862e41eb13b7bac20ec2afe13fb6f1e138eb1d
dikshsawaliya <diksh19csu098@ncuindia.edu>
2021-06-27 12:59:30 +0530

    Sum Array

--- sumarray.java
+++ sumarray.java
@@ -1,17 +1,31 @@
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
-public class sumarray 
-public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
+public class sumarray
+{
+    public static void main (String[]args) 
+    {
+        int n;
+        int sum = 0;
 
-    System.out.println("Enter the size of array: ");
+        System.out.println("Enter the number of elements: ");
+        Scanner sc = new Scanner(System.in);
 
-    int n
-    sum = 0;
+        n = sc.nextInt();
 
-    n = scanner.nextInt();
-    
+        int a[] = new int [n];
+
+        System.out.println("Enter the all the elements: ");
+
+        for (int i=0; i<n; i++)
+        {
+            a[i] = sc.nextInt();
+            sum = sum + a[i];
+
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+
+}
