1c3d436872f93e983f254f7efcd6f8763beacde8
dikshsawaliya <diksh19csu098@ncuindia.edu>
2021-07-10 21:31:34 +0530

    New1

---
+++ percentage.java
@@ -0,0 +1,19 @@
+public class percentage {
+    public static void main(String[] args) {
+        
+        float subject1 = 50f;
+        float subject2 = 6f;
+        float subject3 = 12f;
+        float subject4 = 55f;
+        float subject5 = 40f;
+        float subject6 = 60f;
+
+        System.out.println("Percentage of the given number is " + subject1/60 * 100);
+        System.out.println("Percentage of the given number is " + subject2/60 * 100);
+        System.out.println("Percentage of the given number is " + subject3/60 * 100);
+        System.out.println("Percentage of the given number is " + subject4/60 * 100);
+        System.out.println("Percentage of the given number is " + subject5/60 * 100);
+        System.out.println("Percentage of the given number is " + subject6/60 * 100);
+    }
+    
+}
